84f52b11fdac2bbda58a779ebc10e32d8cac39a6
Picodix <picodix@Peters-MacBook-Pro.local>
2017-03-25 14:17:00 +1300

    add capture one symlink

--- functions.zsh
+++ functions.zsh
@@ -7,4 +7,7 @@ function link-dropbox() {
     ln -s ~/Dropbox/Apps/Atom ~/.atom
     ln -s ~/Dropbox/Apps/Code/app ~/Library/Application\ Support/Code
     ln -s ~/Dropbox/Apps/Code/extensions/.vscode ~/.vscode
+    
+    #save capture one settings
+    #ln -s ~/Dropbox/Apps/Capture\ One ~/Library/Application\ Support
 }
