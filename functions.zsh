5775a695e5d635882d9b22845883af1c56613a63
Picodix <picodix@Peters-MacBook-Pro.local>
2017-03-26 17:43:57 +1300

    update symlink vscode

--- functions.zsh
+++ functions.zsh
@@ -3,11 +3,16 @@ function toLowerCase() {
 }
 
 function link-dropbox() {
+    # Hyper
     ln -s ~/Dropbox/Apps/Hyper/.hyper.js ~/.hyper.js
+
+    # Atom
     ln -s ~/Dropbox/Apps/Atom ~/.atom
+
+    # Visual Studio Code
     ln -s ~/Dropbox/Apps/Code/app ~/Library/Application\ Support/Code
-    ln -s ~/Dropbox/Apps/Code/extensions/.vscode ~/.vscode
-    
-    #save capture one settings
-    #ln -s ~/Dropbox/Apps/Capture\ One ~/Library/Application\ Support
+    ln -s ~/Dropbox/Apps/Code/extensions/.vscode ~/
+
+    # Save capture One settings
+    # ln -s ~/Dropbox/Apps/Capture\ One ~/Library/Application\ Support
 }
